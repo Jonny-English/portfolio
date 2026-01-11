d26bf3863a461031953d67fefc5f05a572741ffc
Alex <git@example.com>
2026-01-11 19:18:04 +0800

    Fix config

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  eslint: {
-    // Checking eslint during build is good, but for static export workflow we might want to be permissive if user hasn't fixed all rules yet.
-    // Let's keep it default.
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
